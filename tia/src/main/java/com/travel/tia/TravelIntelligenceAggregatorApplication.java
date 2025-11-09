5fabaf53f7921b42928e8fd209feab09aece5af6
faridaheydarova <faridaheydarovaa23@gmail.com>
2025-11-09 21:50:10 +0400

    first working version
     Spring Boot 8080 running successfully

---
+++ tia/src/main/java/com/travel/tia/TravelIntelligenceAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.travel.tia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelIntelligenceAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelIntelligenceAggregatorApplication.class, args);
+	}
+
+}
---
+++ tia/src/test/java/com/travel/tia/TravelIntelligenceAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.tia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelIntelligenceAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
